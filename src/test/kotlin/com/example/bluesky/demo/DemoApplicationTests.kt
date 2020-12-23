e606325888885a0957798779510bfcebe34b5cc8
winteul <ulrich.u.winter@daimler.com>
2020-12-23 12:47:10 +0100

    This version runs successfully and sends and receives events to/from an eventhub.

---
+++ src/test/kotlin/com/example/bluesky/demo/DemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.bluesky.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DemoApplicationTests {
+
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
